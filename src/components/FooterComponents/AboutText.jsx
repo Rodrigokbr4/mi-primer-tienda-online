ad7b6c6a8efc9c2a2c5d33630b2d021c6d86d0f4
Rodrigo Cabrera <cabrerarodrigo481@gmail.com>
2023-09-21 14:12:33 -0300

    Probando

--- src/components/FooterComponents/AboutText.jsx
+++ src/components/FooterComponents/AboutText.jsx
@@ -1,7 +1,7 @@
 export function AboutText () {
     return (
         <p className="mx-auto mt-6 max-w-md text-center leading-relaxed text-gray-500">
-            En TiendaX, nuestra pasión es la moda y nuestro compromiso es ayudarte a expresar tu estilo único a través de nuestras prendas cuidadosamente seleccionadas. Somos más que una tienda de ropa; somos un destino para la expresión personal y la confianza en uno mismo. probando
+            En TiendaX, nuestra pasión es la moda y nuestro compromiso es ayudarte a expresar tu estilo único a través de nuestras prendas cuidadosamente seleccionadas. Somos más que una tienda de ropa; somos un destino para la expresión personal y la confianza en uno mismo. probando2
         </p>
     )
 }
